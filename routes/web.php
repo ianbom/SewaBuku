52f2d54948635ff798c4ebee3bba76708afa7ee3
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2025-01-08 17:27:24 +0700

    fix redirect admin

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,22 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\IsAdmin;
 use App\Models\Langganan;
 use App\Models\PaketLangganan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', function () {
-    return redirect()->route('user.buku.index');
-})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        $user = Auth::user();
+        if ($user->is_admin === 1) {
+            return redirect()->route('admin.buku.index');
+        } else {
+            return redirect()->route('user.buku.index');
+        }
+    })->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
